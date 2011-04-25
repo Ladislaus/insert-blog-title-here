39791c049528c8c8b4dc9b40de7ef470c6ae796d
Laszlo Jaeger <pi@laszlojaeger.com>
2011-04-26 01:23:28 +0200

    v1.0.1

---
+++ ss/index.php
@@ -0,0 +1,55 @@
+<?php
+setcookie("Vuser", "Besucher", time()+3600);
+?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+		<title>Ladislaus Blog v.1.0.1</title>
+		<link rel="stylesheet" type="text/css" href="Blog.css">
+	</head>
+<body>
+<div class="header">
+<h2><a href="http://www.laszlojaeger.com/index.php">[insert blog title here]</a></h2>
+</div>
+<div class="content">
+<!-- Hier wird eingesetzt -->
+
+</div>
+<div class="footer">
+<p>Dieser Blog ist in HTML5 und PHP von mir geschrieben.&nbsp;<br>Dies war der  
+<?php
+$datei = fopen("counter.txt","r+");
+$counterstand = fgets($datei, 10);
+if($counterstand == ""){
+  $counterstand = XX;
+}
+$counterstand++;
+echo "<b>$counterstand</b>";
+rewind($datei);
+fwrite($datei, $counterstand);
+fclose($datei);
+?>. Seitenaufruf. <small>(Seit dem XX.XX.XXXX)</small>&nbsp;<br>Und bisher hatte dieser Blog 
+<?php
+if (isset($_COOKIE["Vuser"])) {
+$Vdatei = fopen("visitors.txt","r+");
+$Vcounterstand = fgets($Vdatei, 10);
+echo "<b>$Vcounterstand</b>";
+fclose($Vdatei);
+}
+
+else {
+$Vdatei = fopen("visitors.txt","r+");
+$Vcounterstand = fgets($Vdatei, 10);
+$Vcounterstand++;
+echo "<b>$Vcounterstand</b>";
+rewind($Vdatei);
+fwrite($Vdatei, $Vcounterstand);
+fclose($Vdatei);
+}
+?>
+ Besucher<small> (Seit dem XX.XX.XXXX)</small>&nbsp;<br>
+Dieses Script ist von <a href="http://www.laszlojaeger.com/">Laszlo Jaeger</a>, und unter <a href="https://github.com/Ladislaus/insert-blog-title-here/">github.com ver&ouml;ffentlicht</a>. &nbsp;</p>
+</div>
+</body>
+</html>
