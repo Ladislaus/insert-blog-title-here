5db48b271569b973cd51d4bfd6b09bf2f448fddc
ladislaus <pi@laszlojaeger.com>
2011-07-13 18:37:21 +0200

    add iPhone compatibility for posting new posts

--- edit/index.php
+++ edit/index.php
@@ -1,13 +1,21 @@
 <HTML>
 <HEAD>
 <Title>Admin</Title>
+<meta name = "viewport" content = "width = device-width">
 </HEAD>
 <BODY>
+	<STYLE type="text/css">
+	@media screen and (max-device-width: 480px){
+	body {
+	     width: 220px;
+	}
+	}
+	</STYLE>
 <h1><a href="../index.php">Blog</a></h1>
-<FORM ACTION="write.php" METHOD=POST>
-<textarea type="Text" name="blog" value="" style="width: 100%; height: 300px;"></textarea><br>
+
+<FORM ACTION="write.php" METHOD="POST">
+<textarea enctype="application/x-www-form-urlencoded" name="blog" value="" style="width: 100%; height: 500px;"></textarea><br>
 <div style="text-align:center;"> <input type="Submit" name="senden" value="Senden!" style="width: 90%;"></div>
 </FORM>
-</form>
 </BODY>
 </HTML>
